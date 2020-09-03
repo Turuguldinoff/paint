60550398f7c5787425ba997b94696aa1bc76ff77
turuguldinoff <turuguldinov.i@yandex.ru>
2020-09-04 01:28:35 +0300

    randColorBG

--- js/paint.js
+++ js/paint.js
@@ -7,58 +7,21 @@ const colorRed = document.querySelector('.red')
 const colorBlue = document.querySelector('.blue')
 const colorGreen = document.querySelector('.green')
 
-let color = '#fff'
 
+//let a = ['#fff', 'black', 'red', 'blue', 'green']
 
-colorWhile.addEventListener('click', function() {
-    color = '#fff'
-})
+let b = 16
 
-colorBlack.addEventListener('click', function() {
-    color = 'black'
-})
-
-colorRed.addEventListener('click', function() {
-    color = 'red'
-})
-
-colorBlue.addEventListener('click', function() {
-    color = 'blue'
-})
-
-colorGreen.addEventListener('click', function() {
-    color = 'green'
-})
-ctx.fillStyle = 'black'
-ctx.fillRect(0, 0, 1216, 608)
-
-function paint() {
-    document.addEventListener('mousedown', stac)
-
-    function stac() {
-        document.addEventListener('click', click)
-        document.addEventListener('mousemove', click)
-    }
-
-    function click(event) {
-        ctx.fillStyle = color
-            // ctx.fillRect(event.layerX - 6, event.layerY - 1, 12, 2)
-            // ctx.fillRect(event.layerX - 5, event.layerY - 3, 10, 6)
-            // ctx.fillRect(event.layerX - 4, event.layerY - 4, 8, 8)
-            // ctx.fillRect(event.layerX - 3, event.layerY - 5, 6, 10)
-            // ctx.fillRect(event.layerX - 1, event.layerY - 6, 2, 12)
-
-        let a = 6;
-        ctx.fillRect(event.layerX - a, event.layerY - 1, 2 * a, 2)
-        for (let i = 1; i < a - 2; i++) {
-            ctx.fillRect(event.layerX - (a - i), event.layerY - i - 2, 2 * (a - i), 2 * i + 4)
+function bg() {
+    for (let i = 0; i < 1216 / b; i++) {
+        for (let j = 0; j < 608 / b; j++) {
+            let c = (Math.floor(Math.random() * (16777215 - 0)) + 0).toString(16)
+            ctx.fillStyle = `#${c}` //a[Math.floor(Math.random() * (5 - 0)) + 0]
+            ctx.fillRect(i * b, j * b, b, b)
         }
-        ctx.fillRect(event.layerX - 1, event.layerY - a, 2, 2 * a)
-    }
 
-    document.addEventListener('mouseup', function() {
-        document.removeEventListener('mousemove', click)
-    })
+    }
 }
 
-let paintLayn = setInterval(paint, 0.5)
+
+let paintLayn = setInterval(bg, 0)
